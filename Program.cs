f097a34f2cc1835a5cfdc4496a762aa44659f156
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-04-29 21:11:22 -0300

    Exercício Corrigido 2

--- Program.cs
+++ Program.cs
@@ -1,18 +1,18 @@
 ﻿// Ao realizar um saque um caixa eletrônico calcula a quantidade de notas a entregar ao solicitante.
-Console.WriteLine("--- Caixa Eletrônico ---");
+Console.WriteLine("--- Caixa Eletrônico ---\n");
 
 Console.Write("Digite o valor desejado para saque...:");
-int valorSaque = int.Parse(Console.ReadLine()!);
+double valorSaque = double.Parse(Console.ReadLine()!);
 
 if (valorSaque <= 0) {
     Console.WriteLine("Valor inválido");
 } else {
-    int [] notas = {200, 100, 50, 20, 10, 5, 2, 1};
-    int [] quantidades = new int[notas.Length];
+    double [] notas = {200, 100, 50, 20, 10, 5, 2, 1};
+    double [] quantidades = new double[notas.Length];
 
-    for (int i = 0; i < notas.Length; i++) {
-        quantidades[i] = valorSaque / notas[i];
-        valorSaque %= notas[i];
+    for (double i = 0; i < notas.Length; i++) {
+        quantidades[2] = valorSaque / notas[2];
+        valorSaque %= notas[2];
     }
 
     Console.WriteLine("Notas necessárias para o saque: ");

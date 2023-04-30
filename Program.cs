7ec9d0f72108cf6c68ecd14bc663a38847fa7047
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-04-29 21:11:37 -0300

    Exercício Corrigido 2

--- Program.cs
+++ Program.cs
@@ -2,17 +2,17 @@
 Console.WriteLine("--- Caixa Eletrônico ---\n");
 
 Console.Write("Digite o valor desejado para saque...:");
-double valorSaque = double.Parse(Console.ReadLine()!);
+int valorSaque = int.Parse(Console.ReadLine()!);
 
 if (valorSaque <= 0) {
     Console.WriteLine("Valor inválido");
 } else {
-    double [] notas = {200, 100, 50, 20, 10, 5, 2, 1};
-    double [] quantidades = new double[notas.Length];
+    int [] notas = {200, 100, 50, 20, 10, 5, 2, 1};
+    int [] quantidades = new int[notas.Length];
 
-    for (double i = 0; i < notas.Length; i++) {
-        quantidades[2] = valorSaque / notas[2];
-        valorSaque %= notas[2];
+    for (int i = 0; i < notas.Length; i++) {
+        quantidades[i] = valorSaque / notas[i];
+        valorSaque %= notas[i];
     }
 
     Console.WriteLine("Notas necessárias para o saque: ");

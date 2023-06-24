6b111c0733838b39b30c7ec380c62c4b9b3fa05b
Rickanjo <ricardo.cassettari07@gmail.com>
2023-06-23 21:57:46 -0300

    Exercício em resolução

--- Program.cs
+++ Program.cs
@@ -1,25 +1,78 @@
-﻿// Ao realizar um saque um caixa eletrônico calcula a quantidade de notas a entregar ao solicitante.
-Console.WriteLine("--- Caixa Eletrônico ---\n");
+﻿Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("                   --- Caixa Eletrônico ---\n");
 
-Console.Write("Digite o valor desejado para saque...:");
-int valorSaque = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Escolha um abaixo: ");
+Console.WriteLine("1 - Sacar");
+Console.WriteLine("2 - Depositar");
+char tipoOperação = Console.ReadKey().KeyChar;
 
-if (valorSaque <= 0) {
-    Console.WriteLine("Valor inválido");
-} else {
-    int [] notas = {200, 100, 50, 20, 10, 5, 2, 1};
+Console.WriteLine();
+
+string operaçãoEscolhida;
+
+switch (Char.ToUpper(tipoOperação))
+{
+
+    case '1':
+    operaçãoEscolhida = "Sacar";
+    Console.ForegroundColor = ConsoleColor.Red;
+    break;
+
+    case '2':
+    operaçãoEscolhida = "Depositar";
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    break;
+    
+    default:
+    operaçãoEscolhida = "Operação desconhecida";
+    Console.ForegroundColor = ConsoleColor.DarkGray;
+    break;
+}
+
+Console.WriteLine($"\nVocê escolheu a operação: {operaçãoEscolhida}.");
+
+Console.ResetColor();
+
+Console.WriteLine("Este caixa eletrônico so trabalha com notas de 5 reais ou mais. ");
+Console.WriteLine("Notas Disponiveis para seque 5, 10, 20, 50,100 e 200. ");
+Console.WriteLine("O máximo de dinheiro possivel de sacar 2000 Reais ");
+
+Console.Write("\nDigite o valor desejado para saque...: ");
+int valorSaque;
+bool deuCerto = int.TryParse(Console.ReadLine()!, out valorSaque);
+
+ if (!deuCerto || valorSaque <= 4) 
+ {
+    Console.ForegroundColor =  ConsoleColor.Red;
+    Console.WriteLine("Esse caixa eletrônico não trabalha com esse(s) tipo(s) de nota(s) ");
+
+ } else if (valorSaque > 2000){
+    Console.WriteLine("Esse caixa eletrônico não trabalha com esse(s) tipo(s) de nota(s) ");
+    return;
+
+ } else 
+ {
+    int [] notas = {200, 100, 50, 20, 10, 5,};
     int [] quantidades = new int[notas.Length];
 
-    for (int i = 0; i < notas.Length; i++) {
+    for (int i = 0; i < notas.Length; i++) 
+    {
         quantidades[i] = valorSaque / notas[i];
         valorSaque %= notas[i];
     }
 
     Console.WriteLine("Notas necessárias para o saque: ");
 
-    for (int i = 0; i < notas.Length; i++) {
-        if (quantidades[i] > 0) {
+    for (int i = 0; i < notas.Length; i++) 
+    {
+        if (quantidades[i] > 0) 
+        {
             Console.WriteLine($"{quantidades[i]} nota(s) de {notas[i]} reais");
         }
+
+
+
+
     }
 }
+Console.ResetColor();
